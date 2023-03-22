79cf1683de306fdacdff1f3123b982c3a0d948f1
Zenkiez22 <96461012+Zenkiez22@users.noreply.github.com>
2023-03-22 13:59:37 +0800

    adjust course page

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 @section('content')
-    <div class="container courses mt-2">
+    <div class="container-fluid courses mt-2">
         <div class="row">
             <div class="col-lg-12 margin-tb">
                 <h2>Courses</h2>
