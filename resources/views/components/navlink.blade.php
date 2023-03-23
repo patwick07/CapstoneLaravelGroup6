7545080fbb49aaa683a1e391aa54b6a7d230c014
Zenkiez22 <96461012+Zenkiez22@users.noreply.github.com>
2023-03-23 14:08:57 +0800

    navlinks

--- resources/views/components/navlink.blade.php
+++ resources/views/components/navlink.blade.php
@@ -1,4 +1,4 @@
-<div class="main-container d-flex">
+{{-- <div class="main-container d-flex">
     <div class="sidebar" id="side_nav">
         <div class="header-box px-2 pt-3 pb-4">
             <ul class="list-unstyled px-2 bg-dark">
@@ -17,5 +17,70 @@
             </ul>
         </div>
     </div>
-</div>
+</div> --}}
 
+<div class="container d-flex flex-column">
+    <div class="main-container d-flex">
+        <div class="sidebar" id="side_nav">
+        <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 280px;">
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+          <svg class="bi pe-none me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
+          <span class="fs-4">Kodego University</span>
+        </a>
+        <hr>
+        <ul class="nav nav-pills flex-column mb-auto">
+          <li class="nav-item">
+            <a href="index.php" class="nav-link active" aria-current="page">
+              <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#home"/></svg>
+              Dashboard
+            </a>
+          </li>
+          <li>
+            <a href="/courses" class="nav-link text-white">
+              <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#speedometer2"/></svg>
+              Course
+            </a>
+          </li>
+          <li>
+            <a href="/subjects" class="nav-link text-white">
+              <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#table"/></svg>
+              Subject
+            </a>
+          </li>
+          <li>
+            <a href="/classes" class="nav-link text-white">
+              <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#grid"/></svg>
+              Class
+            </a>
+          </li>
+          <li>
+            <a href="/faculties" class="nav-link text-white">
+              <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#people-circle"/></svg>
+              Faculty
+            </a>
+          </li>
+          <li>
+            <a href="/students" class="nav-link text-white">
+              <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#people-circle"/></svg>
+              Student
+            </a>
+          </li>
+        </ul>
+        <hr>
+        <div class="dropdown">
+          <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
+            <strong>mdo</strong>
+          </a>
+          <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
+            <li><a class="dropdown-item" href="#">New project...</a></li>
+            <li><a class="dropdown-item" href="#">Settings</a></li>
+            <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Sign out</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    </div>
+    </div>
